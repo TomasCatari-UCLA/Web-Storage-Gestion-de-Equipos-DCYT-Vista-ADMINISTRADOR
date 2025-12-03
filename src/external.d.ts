82f03b7d0553a804808c374fb396763e54401445
TomasCatari-UCLA <1001.31401176.ucla@gmail.com>
2025-12-03 11:04:28 -0400

    cambio en la nube

--- src/external.d.ts
+++ src/external.d.ts
@@ -1,4 +1,4 @@
-declare module "https://gtplus.net/forms2/dcytDb/api/Cl_dcytDb.php?v251110-2150" {
+declare module "https://gtplus.net/forms2/dcytDb/api/Cl_dcytDb.php" {
   class Cl_dcytDb {
     constructor({ aliasCuenta } = { aliasCuenta: "" }) {}
 
